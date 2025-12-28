a5e5435d5e64c170c73aed9473868fd7124367e0
Amr Ali <amraliahmed25@gmail.com>
2025-12-28 17:51:20 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "drug-sound-alike-helper",
+  base: "/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
